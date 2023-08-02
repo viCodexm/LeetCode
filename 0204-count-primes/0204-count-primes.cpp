50e88c8afc47cdc1bd4636ce8d2c50cab35e5934
Dmitry <116115112+viCodexm@users.noreply.github.com>
2023-08-02 18:46:15 +0300

    Time: 137 ms (98.25%), Space: 10.2 MB (49.89%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n < 2) return 0;
+        if (n <= 2) return 0;
         vector<bool> w(n, true);
         w[0] = w[1] = false;
-        for (long long div = 2; div < n; ++div)
-            if (w[div])
-                for (long long num = div * div; num < n; num += div)
-                    w[num] = false;
+        int count = 0;
+        for (int div = 2; div < n; ++div)
+            if (w[div]) {
+                count++;
+                long long x = (long long)div * div;
+                if (x < n)
+                    for (int num = x; num < n; num += div)
+                        w[num] = false;
+            }
                 
-        return accumulate(w.begin(), w.end(), 0);
+        return count;
     }
 };
