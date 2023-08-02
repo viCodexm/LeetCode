b0a09dd55bf9c0e95cb35475db902cfb3f0a136b
Dmitry <116115112+viCodexm@users.noreply.github.com>
2023-08-02 23:13:39 +0300

    Time: 16 ms (82.15%), Space: 9.1 MB (58.78%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     vector<int> dirs = {1, 0, -1, 0, 1};
-    int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
-        dp[0][0] = grid[0][0];
-        dfs(0, 0, grid, dp);
-        return dp[n - 1][n - 1];
-    }
     bool inside(int i, int j, int n) {
         return i >= 0 && i < n && j >= 0 && j < n;
     }
-    void dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+    bool dfs(int i, int j, int level, vector<vector<int>>& grid, vector<vector<bool>>& vis) {
+        if (i == grid.size() - 1 && j == grid.size() - 1)
+            return true;
+        if (vis[i][j])
+            return false;
+
+        vis[i][j] = true;
         for (int k = 0; k < 4; ++k) {
             int di = i + dirs[k];
             int dj = j + dirs[k + 1];
-
-            if (inside(di, dj, grid.size()) && dp[di][dj] > max(dp[i][j], grid[di][dj])) {
-                dp[di][dj] = max(dp[i][j], grid[di][dj]);
-                dfs(di, dj, grid, dp);
-            }
+            if (inside(di, dj, grid.size()) && !vis[di][dj] && grid[di][dj] <= level)
+                if (dfs(di, dj, level, grid, vis))
+                    return true;
         }
+        return false;
+    }
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int l = max(grid[0][0], grid[n - 1][n - 1]);
+        int r = n * n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            vector<vector<bool>> vis(n, vector<bool>(n, false));
+            if (dfs(0, 0, mid, grid, vis))
+                r = mid - 1;
+            else l = mid + 1;
+        }
+        return l;
     }
 };
