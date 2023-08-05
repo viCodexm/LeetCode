9b2e4a5c120da5e6198f29ba3da554d4b0de4dbb
Dmitry <116115112+viCodexm@users.noreply.github.com>
2023-08-06 02:10:52 +0300

    Time: 2 ms (46.75%), Space: 6.4 MB (14.92%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        vector<vector<int>> dp(s.size(), vector<int>(s.size(), INT_MAX));
-        return dfs(s, 0, 0, dp);
-    }
-    bool dfs(string& s, int i, int open, vector<vector<int>>& dp) {
-        if (i >= s.size())
-            return open == 0;
-
-        if (open < 0)
-            return false;
-        if (dp[i][open] != INT_MAX)
-            return dp[i][open];
-
-        if (s[i] == '(')
-            return dp[i][open] = dfs(s, i + 1, open + 1, dp);
-        if (s[i] == ')')
-            return dp[i][open] = dfs(s, i + 1, open - 1, dp);
-        if (s[i] == '*')
-            return dp[i][open] = dfs(s, i + 1, open + 1, dp) || dfs(s, i + 1, open - 1, dp) || dfs(s, i + 1, open, dp);
-        
-        return dp[i][open] = false;
+        stack<int> leftBrackets, stars;
+        for (int i = 0; i < s.size(); ++i) {
+            switch (s[i]) {
+            case '(':
+                leftBrackets.push(i);
+                break;
+            case ')':
+                if (!leftBrackets.empty())
+                    leftBrackets.pop();
+                else if (!stars.empty())
+                    stars.pop();
+                else return false;
+                break;
+            case '*':
+                stars.push(i);
+                break;
+            }
+        }
+        while (!leftBrackets.empty() && !stars.empty()) {
+            if (leftBrackets.top() > stars.top())
+                return false;
+            leftBrackets.pop();
+            stars.pop();
+        }
+        return leftBrackets.empty();
     }
 };
