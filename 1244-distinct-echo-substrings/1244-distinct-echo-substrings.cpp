75118f55ab0a6eea9c1133d2b6643d37cbda7fb4
Dmitry <116115112+viCodexm@users.noreply.github.com>
2023-09-09 00:28:48 +0300

    Time: 445 ms (54.59%), Space: 558.9 MB (36.71%) - LeetHub

--- 1244-distinct-echo-substrings/1244-distinct-echo-substrings.cpp
+++ 1244-distinct-echo-substrings/1244-distinct-echo-substrings.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     int distinctEchoSubstrings(string s) {
         int size = s.size();
-        set<string> ans;
+        unordered_set<string> ans;
         for (int windowSize = 1; windowSize <= size / 2; ++windowSize) {
             
             for (int l = 0, r = windowSize, count = 0; l < size - windowSize; ++l, ++r) {
