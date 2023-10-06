d5103348644f9982f4d1ef8f1f0c1b422c1833b1
Dmitry <116115112+viCodexm@users.noreply.github.com>
2023-10-06 18:48:53 +0300

    Time: 0 ms (100%), Space: 6.6 MB (22.36%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -4,12 +4,12 @@ public:
         if (n < 4)
             return n - 1;
         
-        int ans = 1;
-        while (n > 4) {
-            ans *= 3;
-            n -= 3;
+        switch (n % 3) {
+        case 0:
+            return pow(3, n / 3);
+        case 1:
+            return pow(3, n / 3 - 1) * 4;
         }
-
-        return ans * n;
+        return pow(3, n / 3) * 2;
     }
 };
