cd4de38901042ab88d62af7260df54e8649876c8
Dmitry <116115112+viCodexm@users.noreply.github.com>
2023-10-09 20:36:58 +0300

    Time: 3 ms (91.96%), Space: 13.9 MB (37.79%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        return { binary_search(nums, target, true), binary_search(nums, target, false) };
-    }
-    int binary_search(vector<int>& nums, int& target, bool isSearchLeft) {
-        int l = 0, r = nums.size() - 1, idx = -1;
-        while (l <= r) {
-            int mid = (r + l) / 2;
-            if (nums[mid] > target)
-                r = mid - 1;
-            else if (nums[mid] < target)
-                l = mid + 1;
-            else {
-                idx = mid;
-                if (isSearchLeft)
-                    r = mid - 1;
-                else l = mid + 1;
-            }
-        }
-        return idx;
+        const int l = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        if (l == nums.size() || nums[l] != target)
+            return { -1, -1 };
+        const int r = upper_bound(nums.begin(), nums.end(), target) - nums.begin() - 1;
+        return { l, r };
     }
 };
