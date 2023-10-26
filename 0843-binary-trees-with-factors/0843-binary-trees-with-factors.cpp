1a792f8dffab1f5b538a3d574f6b31276545a2d5
Dmitry <116115112+viCodexm@users.noreply.github.com>
2023-10-27 00:19:59 +0300

    Time: 22 ms (95.28%), Space: 10.1 MB (36.61%) - LeetHub

---
+++ 0843-binary-trees-with-factors/0843-binary-trees-with-factors.cpp
@@ -0,0 +1,32 @@
+const int modulo = 1e9 + 7;
+
+class Solution {
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        unordered_set<int> s(arr.begin(), arr.end());
+        unordered_map<int, int> dp;
+        for (int x : arr) dp[x] = 1;
+        
+        for (int i : arr) {
+            for (int j : arr) {
+                if (j > sqrt(i))
+                    break;
+                if (i % j == 0 && s.find(i / j) != s.end()) {
+                    long long temp = (long long)dp[j] * dp[i / j];
+                    if (i / j == j) {
+                        dp[i] = (dp[i] + temp) % modulo;
+                    } else {
+                        dp[i] = (dp[i] + temp * 2) % modulo;
+                    }
+                }
+            }
+        }
+        
+        int result = 0;
+        for (auto& [_, val] : dp)
+            result = (result + val) % modulo;
+        
+        return result;
+    }
+};
