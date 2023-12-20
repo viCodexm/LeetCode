9c3c448a209b984dda05faeb4adbf87ad8331e60
Dmitry <116115112+viCodexm@users.noreply.github.com>
2023-12-20 07:48:10 +0300

    Time: 24 ms (5.34%), Space: 46.9 MB (94.77%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        const int size = prices.size();
-        int min1 = INT_MAX, min2 = INT_MAX;
-        for (int i = 0; i < size; ++i) {
-            if (prices[i] <= min1) {
-                min2 = min1;
-                min1 = prices[i];
-            } else if (prices[i] < min2)
-                min2 = prices[i];
-        }
-        const int sum = min1 + min2;
-        return sum <= money ? money - sum : money;
+        partial_sort(prices.begin(), prices.begin() + 2, prices.end());
+        const int sum = money - prices[0] - prices[1];
+        return sum >= 0 ? sum : money;
     }
 };
