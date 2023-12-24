03af31bce2d0f38a29d5251830e28d6401d589ee
Dmitry <116115112+viCodexm@users.noreply.github.com>
2023-12-24 17:57:02 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int a = 0, b = 0;
+        int s01 = 0;
         const int size = s.size();
         for (int i = 0; i < size; ++i)
-            if (s[i] == '1')
-                if (i & 1)
-                    a++;
-                else b++;
-            else if (i & 1)
-                    b++;
-                else a++;
-        return min(a, b);
+            s01 += i & 1 && s[i] == '1';
+        return min(size - s01, s01);
     }
 };
