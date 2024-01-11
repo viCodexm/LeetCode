d3bc1bb4720008be5a95f432b453fa953b47b33f
Dmitry <116115112+viCodexm@users.noreply.github.com>
2024-01-11 21:37:30 +0300

    Time: 15 ms (56.68%), Space: 19.9 MB (68.18%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0, votes = 0;
+        for (int& num : nums) {
+            if (candidate == num)
+                votes++;
+            else {
+                votes--;
+                if (votes <= 0) {
+                    candidate = num;
+                    votes = 1;
+                }
+            }
+        }
+        return candidate;
+    }
+};
