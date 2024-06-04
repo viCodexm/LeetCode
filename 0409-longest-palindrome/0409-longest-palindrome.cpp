0b593a4c6e5e1ba792fc0e3e68efd87cd6afe087
Dmitry <116115112+viCodexm@users.noreply.github.com>
2024-06-04 15:50:24 +0300

    Time: 0 ms (100%), Space: 9 MB (26.04%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -5,12 +5,10 @@ public:
         for (char c : s)
             dict[c]++;
 
-        int sum = 0, max = 0, addOne = 0;
-        for (auto e : dict) {
-            sum += e.second - (e.second & 1);
-            addOne |= (e.second & 1);
-        }
+        int odd = 0;
+        for (auto e : dict)
+            odd += e.second & 1;
 
-        return sum + max + addOne;
+        return s.size() - odd + (odd > 0);
     }
 };
