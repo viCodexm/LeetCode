b453e6bcaddc1ae81ad71b839835cdc65eea9a52
Dmitry <116115112+viCodexm@users.noreply.github.com>
2025-09-01 11:05:52 +0300

    Time: 283 ms (77.49%), Space: 97.7 MB (81.29%) - LeetHub

--- 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-        		auto compare = [](const pair<int, int>& left, const pair<int, int>& right) {
-			const double l1		 = ((double)left.first / left.second);
-			const double l2		 = ((double)(left.first + 1) / (left.second + 1));
-			const double lgrowth = l2 - l1;
-
-			const double r1		 = ((double)right.first / right.second);
-			const double r2		 = ((double)(right.first + 1) / (right.second + 1));
-			const double rgrowth = r2 - r1;
-
-			return lgrowth < rgrowth;
+        priority_queue<pair<double, int>> pq;
+        auto gain = [](int pass, int total) {
+            return ((double)pass + 1) / (total + 1) - ((double)pass / total);
 		};
-		priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(compare)> pq;
 		const int n = classes.size();
 		for (int i = 0; i < n; ++i) {
-			pq.push({classes[i][0], classes[i][1]});
+			pq.push({gain(classes[i][0], classes[i][1]), i});
 		}
 
 		for (int i = 0; i < extraStudents; ++i) {
-			pair<int, int> x = pq.top();
+			const auto [profit, idx] = pq.top();
 			pq.pop();
-			pq.push({x.first + 1, x.second + 1});
+            classes[idx][0]++;
+            classes[idx][1]++;
+			pq.push({gain(classes[idx][0], classes[idx][1]), idx});
 		}
 		double answer = 0;
-		while (!pq.empty()) {
-			const auto [x, y] = pq.top();
-			answer += (double)x / y;
-			pq.pop();
+		for (const vector<int>& room : classes) {
+			answer += (double)room[0] / room[1];
 		}
 		return answer / n;
     }
