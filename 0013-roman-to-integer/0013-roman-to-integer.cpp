70ef7988448c1a2141dd3892eb9c071f4321ee68
Dmitry <116115112+viCodexm@users.noreply.github.com>
2025-09-05 20:43:14 +0300

    Time: 4 ms (67.04%), Space: 10.2 MB (77.94%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-        unordered_map<char, int> dict = {
+    int romanToInt(string_view s) {
+        static unordered_map<char, int> dict = {
             {'M', 1000}, {'D', 500}, {'C', 100},
             {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1}
         };
-        int ans = 0;
+        int ans = 0, cur = 0, next = 0;
         const int size = s.size() - 1;
         for (int i = 0; i < size; ++i) {
-            ans += (dict[s[i]] < dict[s[i + 1]]) ? -dict[s[i]] : dict[s[i]];
+            cur = dict[s[i]];
+            next = dict[s[i + 1]];
+            ans += (cur < next) ? -cur : cur;
         }
         ans += dict[s.back()];
         return ans;
